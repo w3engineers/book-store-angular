1e8ed0d4042db370888e0e0c6ab596f6adf40bed
pankaj kumar singh <travelopediallc@outlook.com>
2022-11-21 19:16:33 +0530

    app init

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes:Routes=[
+  {path:'about-us',component:AboutUsComponent},
+  {path:'how-it-works',component:HowItWorksComponent}
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowItWorksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
