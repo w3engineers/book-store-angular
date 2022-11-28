d32d7be9e7afd7dd1189fc2632b571a6bb8748c9
pankaj kumar singh <travelopediallc@outlook.com>
2022-11-28 19:13:08 +0530

    init start

--- bookStoreApp/src/app/app.component.ts
+++ bookStoreApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  email='pankajonline2u@gmail.com';
   title = 'bookStoreApp';
 }
--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,25 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
-import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PublicModule } from './public/public.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const routes:Routes=[
-  {path:'about-us',component:AboutUsComponent},
-  {path:'how-it-works',component:HowItWorksComponent}
 
-];
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     HowItWorksComponent
+
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
+    BrowserModule,   
+    AuthModule,
+    UserModule,
+    PublicModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+
+    
   ],
   exports:[
     RouterModule
